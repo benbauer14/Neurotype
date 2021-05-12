d1d727b93c84bd1c582e0a59d4e3aaaaee67ebbb
Ben Bauer <ben.bauer1@gmail.com>
2021-05-12 11:53:50 -0500

    created get and post routes for patients

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useState, Component } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-// This is one of our simplest components
-// It doesn't have local state, so it can be a function component.
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is, so it doesn't need 'connect()'
+//this page is used to test dispatches and store retrieval.
 
-const AboutPage = () => (
-  <div className="container">
-    <div>
-      <p>This about page is for anyone to read!</p>
-    </div>
-  </div>
-);
+
+const AboutPage = () => {
+
+  const dispatch = useDispatch()
+  const participants = useSelector((store) => store.participants);
+
+  const getPart = () => {
+    dispatch({type: 'FETCH_PARTICIPANTS'})
+  }
+  const postPart = () => {
+    const patient = {
+      name: "Bob",
+      Gender: "M",
+      birthdate: "1/1/2000",
+      height: 70,
+      weight: 200
+    }
+    console.log(patient)
+    dispatch({type: 'POST_PARTICIPANT', payload: patient})
+  }
+
+    return (
+      <>
+      <div>
+        <p>This page is used to test dispatches and store retrieval</p>
+        <button onClick={() => getPart()}>Get em</button>
+        <button onClick={() => postPart()}>Post em</button>
+        {JSON.stringify(participants)}
+
+      </div>
+      </>
+    );
+  
+}
 
 export default AboutPage;
