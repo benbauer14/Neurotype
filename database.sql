0d6acf8384a8e103571426a10e70497eb343de0a
Ben Bauer <ben.bauer1@gmail.com>
2021-05-12 10:36:11 -0500

    updated database.sql, updated pool to point to  database named neurosity

--- database.sql
+++ database.sql
@@ -1,10 +1,81 @@
 
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
-CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+-- database name: neurosity
+
+CREATE TABLE "group" (
+    id SERIAL PRIMARY KEY UNIQUE,
+    name text,
+    description text
 );
+
+CREATE TABLE "user" (
+    id SERIAL PRIMARY KEY,
+    email text,
+    name text,
+    password text,
+    role text,
+    group_id integer REFERENCES "group"(id)
+);
+
+CREATE TABLE participant (
+    id SERIAL PRIMARY KEY,
+    name text,
+    gender text,
+    birthdate text,
+    height text,
+    weight integer,
+    session_id integer REFERENCES session(id)
+);
+
+CREATE TABLE participant (
+    id SERIAL PRIMARY KEY,
+    name text,
+    gender text,
+    birthdate text,
+    height text,
+    weight integer,
+    session_id integer REFERENCES session(id)
+);
+
+CREATE TABLE session (
+    id SERIAL PRIMARY KEY,
+    time timestamp with time zone,
+    notes text,
+    user_id integer REFERENCES "user"(id),
+    participant_id integer REFERENCES participant(id),
+    group_id integer REFERENCES "group"(id)
+);
+
+CREATE TABLE eegproc (
+    id SERIAL PRIMARY KEY UNIQUE,
+    task text,
+    acquired text,
+    reactivityprofile text,
+    reactivityscore text,
+    session_id integer REFERENCES session(id)
+);
+
+CREATE TABLE eegraw (
+    id SERIAL PRIMARY KEY,
+    task text,
+    acquired text,
+    run text,
+    json text,
+    session_id integer REFERENCES session(id)
+);
+
+CREATE TABLE surveyproc (
+    id SERIAL PRIMARY KEY,
+    cravignscore text,
+    depressionscore text,
+    anxietyscore text,
+    usestatus text,
+    session_id integer REFERENCES session(id)
+);
+
+CREATE TABLE surveyraw (
+    id SERIAL PRIMARY KEY,
+    variant text,
+    json text,
+    session_id integer REFERENCES session(id)
+);
+
