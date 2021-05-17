7396ca512081886a19cbfd25a094576c5b8d91f9
Ben Bauer <ben.bauer1@gmail.com>
2021-05-17 16:18:57 -0500

    updated login page

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,5 +1,5 @@
-import React, { Component, useEffect  } from 'react';
-import { connect, useDispatch } from 'react-redux';
+import React, { Component } from 'react';
+import { connect} from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 import LoginForm from '../LoginForm/LoginForm';
 
@@ -19,18 +19,6 @@ class LoginPage extends Component {
     return (
       <div>
         <LoginForm />
-
-        <center>
-          <button
-            type="button"
-            className="btn btn_asLink"
-            onClick={() => {
-              this.props.history.push('/registration');
-            }}
-          >
-            Register Participant
-          </button>
-        </center>
       </div>
     );
   }
