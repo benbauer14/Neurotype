f8bb536579b487bb048009447f3fa69df05df489
MatthewHeinrich <mheinrich95@gmail.com>
2021-05-17 12:46:06 -0500

    styling table and buttons for select patient

--- src/components/CheckInPage/CheckInPage.js
+++ src/components/CheckInPage/CheckInPage.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import mapStoreToProps from '../../redux/mapStoreToProps';
+import Button from '@material-ui/core/Button';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 
 // Basic functional component structure for React with default state
 // value setup. When making a new component be sure to replace the
@@ -11,16 +13,57 @@ function CheckIn(props) {
     // a default value of 'Functional Component'
     //   const [heading, setHeading] = useState('Functional Component');
 
+    const BootstrapButton = withStyles({
+        root: {
+            boxShadow: '2px 2px 2px 1px rgba(0, 0, 0, 0.2);',
+            textTransform: 'none',
+            textDecoration: 'none',
+            fontSize: 16,
+            padding: '6px 12px',
+            border: '1px solid',
+            lineHeight: 1.5,
+            backgroundColor: 'rgb(32, 115, 136)',
+            borderColor: 'rgb(32, 115, 136)',
+            color: 'white',
+            fontFamily: [
+                '-apple-system',
+                'BlinkMacSystemFont',
+                '"Segoe UI"',
+                'Roboto',
+                '"Helvetica Neue"',
+                'Arial',
+                'sans-serif',
+                '"Apple Color Emoji"',
+                '"Segoe UI Emoji"',
+                '"Segoe UI Symbol"',
+            ].join(','),
+            '&:hover': {
+                backgroundColor: '#0069d9',
+                borderColor: '#0062cc',
+                boxShadow: 'none',
+            },
+            '&:active': {
+                boxShadow: 'none',
+                backgroundColor: '#0062cc',
+                borderColor: '#005cbf',
+            },
+            '&:focus': {
+                boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+            },
+        },
+    })(Button);
+
+
     return (
         <>
             <h3>{JSON.stringify(props.match.params.id)}</h3>
             <div>
                 <Link to="/questionnaire">
-                    <button>Check In</button>
+                    <BootstrapButton>Check In</BootstrapButton>
                 </Link>
-                <button>Photo Activity</button>
+                <BootstrapButton>Photo Activity</BootstrapButton>
                 <Link to="/dashboard">
-                    <button>Dashboard</button>
+                    <BootstrapButton>Dashboard</BootstrapButton>
                 </Link>
                 
             </div>
--- src/components/SelectParticipant/SelectParticipant.jsx
+++ src/components/SelectParticipant/SelectParticipant.jsx
@@ -8,26 +8,40 @@ import { CSVLink } from 'react-csv'
 import Select from 'react-select';
 import './SelectParticipant.css'
 
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+// import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Checkbox from '@material-ui/core/Checkbox';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+// import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+// import SaveIcon from '@material-ui/icons/Save';
+import {IoCloudDownload} from 'react-icons/io5';
+
 
 const SelectParticipant = () => {
     const dispatch = useDispatch();
     const participants = useSelector(store => store.participants)
 
     useEffect(() => {
-        dispatch({type: "FETCH_PARTICIPANTS"})
-    },[])
+        dispatch({ type: "FETCH_PARTICIPANTS" })
+    }, [])
 
     console.log('Participants', participants)
 
-    
+
     let participantData = []
     console.log(participantData)
 
-    participants.map( participant => {
-        participantData.push( participant.name )
+    participants.map(participant => {
+        participantData.push(participant.name)
     })
     const BarStyling = { width: "20rem", background: "#F2F1F9", border: "none", padding: "0.5rem" };
-    
+
     let search = '';
     // this is the local state for the list of sna objects that matched the search
     const [filtered, setFiltered] = useState([]);
@@ -35,26 +49,26 @@ const SelectParticipant = () => {
     const [searched, setSearched] = useState(false);
     // conditionally renders the list of all natural areas or the filtered natural areas
     const displayList = () => {
-        let display = participants 
-        if( searched ) {
-            display = filtered 
+        let display = participants
+        if (searched) {
+            display = filtered
         }
         return display;
     }
     // handles search f
     const searchForParticipant = (e) => {
         search += e.target.value;
-        console.log( 'search', search );
+        console.log('search', search);
         // create a regex pattern that looks at each letter of the search query
-        if( search.length > 0) {
-            let pattern = search.split('').map( x => {
-            return `(${x})`
+        if (search.length > 0) {
+            let pattern = search.split('').map(x => {
+                return `(${x})`
             }).join('');
             // creates a regex based on the search pattern and looks globally and is case insensitive
             let regex = new RegExp(`${pattern}`, "gi");
             console.log(regex);
             // sets the filtered array equal to the sna's that match the query
-            setFiltered(participants.filter( participant => (participant.name + ' ').match(regex)))
+            setFiltered(participants.filter(participant => (participant.name + ' ').match(regex)))
             console.log(filtered);
             // set the search state to the opposite of what it was
             setSearched(true);
@@ -62,68 +76,144 @@ const SelectParticipant = () => {
             setSearched(false);
         }
     }
-    
+
+    const useStyle = makeStyles => ({
+        table: {
+            minWidth: 450,
+            maxWidth: 650
+        },
+    });
+
+    const classes = useStyle();
+
+    const StyledTableCell = withStyles((theme) => ({
+        head: {
+            backgroundColor: theme.palette.common.white,
+            color: theme.palette.common.white,
+        },
+        body: {
+            fontSize: 16,
+        },
+    }))(TableCell);
+
+    const StyledTableRow = withStyles((theme) => ({
+        root: {
+            '&:nth-of-type(odd)': {
+                backgroundColor: theme.palette.action.hover,
+            },
+        },
+    }))(TableRow);
+
+    const BootstrapButton = withStyles({
+        root: {
+            boxShadow: '2px 2px 2px 1px rgba(0, 0, 0, 0.2);',
+            textTransform: 'none',
+            textDecoration: 'none',
+            fontSize: 16,
+            padding: '6px 12px',
+            border: '1px solid',
+            lineHeight: 1.5,
+            backgroundColor: 'rgb(32, 115, 136)',
+            borderColor: 'rgb(32, 115, 136)',
+            color: 'white',
+            fontFamily: [
+                '-apple-system',
+                'BlinkMacSystemFont',
+                '"Segoe UI"',
+                'Roboto',
+                '"Helvetica Neue"',
+                'Arial',
+                'sans-serif',
+                '"Apple Color Emoji"',
+                '"Segoe UI Emoji"',
+                '"Segoe UI Symbol"',
+            ].join(','),
+            '&:hover': {
+                backgroundColor: '#0069d9',
+                borderColor: '#0062cc',
+                boxShadow: 'none',
+            },
+            '&:active': {
+                boxShadow: 'none',
+                backgroundColor: '#0062cc',
+                borderColor: '#005cbf',
+            },
+            '&:focus': {
+                boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+            },
+        },
+    })(Button);
+
+
 
     return (
         <>
-        <div>
-        <h2>Select Participant</h2>
-        <input
-            className="searchBar"
-            style={BarStyling}
-            key="random1"
-            // value={keyword}
-            placeholder={"search trails"}
-            onChange={(e) => searchForParticipant(e)}
-        />
-        <button>Download</button>
-        <table>
-            <th>
-                <tr>Name</tr>
-            </th>
-            <th>
+            <div>
+                <h1 className="selectPart">Select Participant</h1>
+                <input // search bar for participants 
+                    className="searchBar"
+                    style={BarStyling}
+                    key="random1"
+                    // value={keyword}
+                    placeholder={"search"}
+                    onChange={(e) => searchForParticipant(e)}
+                />
+                
+                <CSVLink className="csvButton" data={participants} filename={"all-patients-info.csv"}><BootstrapButton variant="contained"
+                    color="default"><IoCloudDownload value={{ style: { verticalAlign: 'middle' } }} ></IoCloudDownload> Download CSV</BootstrapButton></CSVLink>
+                <Table className={classes.table} aria-label="customized table">
+                    <TableHead component='th'>
+                        <TableRow>
+                            <TableCell align="center">Participant Name</TableCell>
+                            <TableCell align="center">Group #</TableCell>
+                            <TableCell align="center">Select</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    {/* <TableHead className="tableHead">
                 <tr>Select</tr>
-            </th>
-                { !searched ?
-                participants.map(participant => {
-                    return (
-                        <>
-                            
-                                <tbody>
-                                    <tr>
-                                        <td>{participant.name}</td> 
-                                        <td><Link to={`/checkin/${participant.name}`}><button>Select</button></Link></td>
-                                    </tr>
-                                </tbody>
-                        </>
-                    )
+            </TableHead> */}
+                    {!searched ? // renders all participants 
+                        participants.map(participant => {
+                            return (
+                                <>
 
-                })
-                
-                :
-                
-                filtered.map(filter => {
-                    return(
-                        <>
-                            
-                        <tbody>
-                            <tr>
-                                <td>{filter.name}</td> 
-                                <td><Link to={`/checkin/${filter.name}`}><button>Select</button></Link></td>
-                            </tr>
-                        </tbody>
-                        </>
-                    )
-    })
-    }
+                                    <TableBody className='hover={true}' component={Paper}>
+                                        <TableRow hover={true}>
+                                            <StyledTableCell align="center" scope="row">{participant.name}</StyledTableCell>
+                                            <StyledTableCell align="center" scope="row">{participant.group_id}</StyledTableCell>
+                                            <StyledTableCell align="center"><Link to={`/checkin/${participant.name}`}><BootstrapButton>Select</BootstrapButton></Link></StyledTableCell>
+                                        </TableRow>
+                                    </TableBody>
+                                </>
+                            )
 
-        </table>
-        
-        <CSVLink data={participants} filename={"all-patients-info.csv"}><button>Download CVS</button></CSVLink>
+                        })
+
+                        : // if searched, render filtered participants
+
+                        filtered.map(filter => {
+                            return (
+                                <>
+
+                                    <TableBody component={Paper}>
+                                        <TableRow >
+                                            <StyledTableCell align="center">{filter.name}</StyledTableCell>
+                                            <StyledTableCell align="center" scope="row">{filter.group_id}</StyledTableCell>
+                                            <StyledTableCell align="center"><Link to={`/checkin/${filter.name}`}><button>Select</button></Link></StyledTableCell>
+                                        </TableRow>
+                                    </TableBody>
+                                </>
+                            )
+                        })
+                    }
+
+                </Table>
+
+                {/* <CSVLink data={participants} filename={"all-patients-info.csv"}><button>Download CSV</button></CSVLink> */}
 
 
 
-        </div>
+            </div>
         </>
     )
 }
