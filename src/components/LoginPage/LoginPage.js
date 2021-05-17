ad1fdeac93a0baf3cbbb64eb1b7e1bc336b85cb1
alexbraatz <alexjbraatz@gmail.com>
2021-05-17 14:43:12 -0500

    convert classses to functions that can use hooks

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,39 +1,38 @@
-import React, { Component, useEffect  } from 'react';
-import { connect, useDispatch } from 'react-redux';
-import mapStoreToProps from '../../redux/mapStoreToProps';
+import React, { useEffect  } from 'react';
+import { useHistory } from 'react-router-dom' 
+import { useDispatch } from 'react-redux';
 import LoginForm from '../LoginForm/LoginForm';
 
-class LoginPage extends Component {
 
-  //    pageSet = () => {
-  //   const dispatch = useDispatch()
-  //   dispatch({type: 'SET_PAGE', payload: "LOGIN"})
-  // }
+const LoginPage = () => {
 
+  const dispatch = useDispatch()
+  const history = useHistory()
+    
 
+  useEffect( () => {
+    dispatch({type: 'SET_PAGE', payload: "LOGIN"})
+  }, [] );
 
-  render() {
-  //   useEffect(() => {
-  //     this.pageSet()
-  // },[])
-    return (
-      <div>
-        <LoginForm />
-
-        <center>
-          <button
-            type="button"
-            className="btn btn_asLink"
-            onClick={() => {
-              this.props.history.push('/registration');
-            }}
-          >
-            Register Participant
-          </button>
-        </center>
-      </div>
-    );
+  const goToRegistration = () => {
+    history.push('/registration');
   }
+
+  return (
+    <div>
+      <LoginForm />
+
+      <center>
+        <button
+          type="button"
+          className="btn btn_asLink"
+          onClick={goToRegistration}
+        >
+          Register Participant
+        </button>
+      </center>
+    </div>
+  );
 }
 
-export default connect(mapStoreToProps)(LoginPage);
+export default LoginPage;
