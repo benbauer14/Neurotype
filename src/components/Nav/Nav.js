4688cff01eef9cb56d3d7d584247ddf2e95b2932
MatthewHeinrich <mheinrich95@gmail.com>
2021-05-20 16:48:55 -0500

    nav bar displaying participant

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -9,7 +9,7 @@ import { IconContext } from "react-icons";
 
 const Nav = (props) => {
   const page = useSelector((store) => store.page);
-  const participant = useSelector((store) => store.currentsession.rows)
+  const participant = useSelector((store) => store.currentsession.participant_name)
   
   let loginLinkData = {
     path: '/login',
@@ -22,9 +22,51 @@ const Nav = (props) => {
   }
   console.log(participant)
  
-  // if (participant === null){
+  if (participant != null){
   return (
     <div className="nav">
+      <div className="homelink">
+      <Link to="/home">
+        <h2 className="nav-title">neurotype</h2>
+      </Link>
+      </div>
+      <div className="pageName">
+      <h2>{page}</h2>
+      </div>
+      <div className="nav-right">
+        {/* <Link className="nav-link" to={loginLinkData.path}>
+          Show this link if they are logged in or not,
+          but call this link 'Home' if they are logged in,
+          and call this link 'Login / Register' if they are not
+          {loginLinkData.text}
+        </Link> */}
+        {/* Show the link to the info page and the logout button if the user is logged in */}
+        {props.store.user.id && (
+          <>
+            {/* <Link className="nav-link" to="/about">
+              About
+            </Link>
+            <Link className="nav-link" to="/info">
+              Info Page
+            </Link> */}
+            <p className="participantNav">Participant ID: <strong>{participant}</strong></p>
+            <LogOutButton className="nav-link" >
+              <IconContext.Provider value={{size: '10em'}}>
+                <>
+                  <IoLogOutOutline></IoLogOutOutline>
+                </>
+              </IconContext.Provider>
+            </LogOutButton>
+          </>
+        )}
+        {/* Always show this link since the about page is not protected */}
+        
+      </div>
+    </div>
+  )} else {
+    return(
+      <>
+        <div className="nav">
       <div className="homelink">
       <Link to="/home">
         <h2 className="nav-title">neurotype</h2>
@@ -62,7 +104,9 @@ const Nav = (props) => {
         
       </div>
     </div>
-  );
+      </>
+    )
+  }
           
 };
 
