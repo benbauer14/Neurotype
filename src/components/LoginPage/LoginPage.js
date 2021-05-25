a4b5af65a54c55f4aaf714cd32fd2fb1daa6b236
Nate Treimer <natetreimer@gmail.com>
2021-05-25 12:55:24 -0500

    fixed login

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -23,16 +23,6 @@ const LoginPage = () => {
   return (
     <div>
       <LoginForm />
-
-      <center>
-        <button
-          type="button"
-          className="btn btn_asLink"
-          onClick={goToRegistration}
-        >
-          Register Participant
-        </button>
-      </center>
     </div>
   );
 }
