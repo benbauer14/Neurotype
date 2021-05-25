743b9008fe1546ed8ca27a25d52e7a6625d4fa50
MatthewHeinrich <mheinrich95@gmail.com>
2021-05-25 10:04:33 -0500

    graphs made for brain data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -30,6 +30,7 @@ import RegisterPage from '../RegisterPage/RegisterPage';
 import Survey from '../../neuromarkr-demo-survey/src/components/surveyDisplays/surveyone'
 import AdminEditPage from '../AdminEditPage/AdminEditPage';
 import EditUserInfo from '../EditUserInfo/EditUserInfo';
+import LineChart from '../LineChart/LineChart';
 
 
 // import experiment from '../../neuromarkr-demo-pictureviewing/src/studies/pictureviewing'
@@ -135,6 +136,11 @@ class App extends Component {
             Visiting localhost:3000/user will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
             Even though it seems like they are different pages, the user is always on localhost:3000/user */}
+            <ProtectedRoute
+              exact
+              path="/graph"
+              component={LineChart}
+            />
             <ProtectedRoute
               // logged in shows UserPage else shows LoginPage
               exact
