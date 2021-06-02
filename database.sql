8437cb1d2c647cbd14a744f57eb151f878de615a
Ben Bauer <ben.bauer1@gmail.com>
2021-06-02 15:03:54 -0500

    updated database.sql to ensure it reflects final database

--- database.sql
+++ database.sql
@@ -1,48 +1,6 @@
 
 -- database name: neurosity
 
-CREATE TABLE "researchgroup" (
-    id SERIAL PRIMARY KEY UNIQUE,
-    name text,
-    description text
-);
-
-CREATE TABLE "user" (
-    id SERIAL PRIMARY KEY,
-    email text,
-    name text,
-    password text,
-    role text,
-    group_id integer REFERENCES "group"(id),
-    disabled boolean DEFAULT false
-);
-
-CREATE TABLE participant (
-    id SERIAL PRIMARY KEY,
-    name text,
-    gender text,
-    birthdate text,
-    height text,
-    weight integer,
-    group_id integer,
-    disabled boolean DEFAULT false
-);
-
-CREATE TABLE pins (
-	id SERIAL PRIMARY KEY,
-	pin_number integer,
-	group_id integer REFERENCES "group"(id)
-);
-
-CREATE TABLE session (
-    id SERIAL PRIMARY KEY,
-    time timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
-    notes text,
-    user_id integer REFERENCES "user"(id),
-    participant_id integer REFERENCES participant(id),
-    group_id integer REFERENCES "group"(id)
-);
-
 CREATE TABLE eegproc (
     id SERIAL PRIMARY KEY UNIQUE,
     task text,
@@ -61,9 +19,41 @@ CREATE TABLE eegraw (
     session_id integer REFERENCES session(id)
 );
 
+CREATE TABLE participant (
+    id SERIAL PRIMARY KEY,
+    name text,
+    gender text,
+    birthdate text,
+    height text,
+    weight integer,
+    group_id integer REFERENCES researchgroup(id),
+    disabled boolean DEFAULT false
+);
+
+CREATE TABLE pins (
+    id SERIAL PRIMARY KEY,
+    pin_number integer,
+    group_id integer REFERENCES researchgroup(id)
+);
+
+CREATE TABLE researchgroup (
+    id SERIAL PRIMARY KEY,
+    name text,
+    description text
+);
+
+CREATE TABLE session (
+    id SERIAL PRIMARY KEY,
+    time timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    notes text,
+    user_id integer REFERENCES users(id),
+    participant_id integer REFERENCES participant(id),
+    group_id integer REFERENCES researchgroup(id)
+);
+
 CREATE TABLE surveyproc (
     id SERIAL PRIMARY KEY,
-    cravignscore text,
+    cravingscore text,
     depressionscore text,
     anxietyscore text,
     usestatus text,
@@ -74,6 +64,17 @@ CREATE TABLE surveyraw (
     id SERIAL PRIMARY KEY,
     variant text,
     json text,
-    session_id integer REFERENCES session(id)
+    session_id integer REFERENCES session(id),
+    csvlocation text
+);
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email text,
+    name text,
+    password text,
+    role text,
+    group_id integer REFERENCES researchgroup(id),
+    disabled boolean DEFAULT false
 );
 
